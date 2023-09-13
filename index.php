06dde26b05e06b93c022798a028c025de5a01db6
Cedric Baaklini <cabaaklini@icloud.com>
2023-09-13 01:40:20 -0400

    Adding some things

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 <html lang = "en">
     <head>
         <meta charset = "UTF-8">
-        <title>Title</title>
-
+        <title>Docker Demo</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
 
         <?php
 
-        echo "Hello world";
+        echo "<h1>Hello Docker!</h1>";
+        
+        $host = 'localhost';
+        $user = 'root';
+        $pass = 'password';
+        
+        $conn = new mysqli($host, $user, $pass);
+        
+        if ($conn -> connect_error) {
+            die("Connection failed: " . $conn -> connect_error);
+        } else {
+            echo "<p> Connected to MySQL server successfully! </p>";
+        }
 
         ?>
 
